8b206e143cb1b5f2a4880de74ef71e5b902ccb5c
Alexander Ryakhin <bonney977@gmail.com>
2021-04-15 17:19:06 +0300

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Alexander Bonney on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
